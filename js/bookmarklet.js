d63185410d61ef04646d687d774fdcd8c8082879
Curiositry <isa@automathic.org>
2025-01-02 10:06:08 -0800

    Expand to word boundary to prevent invalid text fragment links

--- js/bookmarklet.js
+++ js/bookmarklet.js
@@ -1,4 +1,53 @@
 javascript: (function() {
+
+function snapSelectionToWord() {
+  // this function courtesy of Tim Down (author of Rangy library), creative commons licence
+  var sel;
+
+    // Check for existence of window.getSelection() and that it has a
+    // modify() method. IE 9 has both selection APIs but no modify() method.
+    if (window.getSelection && (sel = window.getSelection()).modify) {
+        sel = window.getSelection();
+        if (!sel.isCollapsed) {
+
+            // Detect if selection is backwards
+            var range = document.createRange();
+            range.setStart(sel.anchorNode, sel.anchorOffset);
+            range.setEnd(sel.focusNode, sel.focusOffset);
+            var backwards = range.collapsed;
+            range.detach();
+
+            // modify() works on the focus of the selection
+            var endNode = sel.focusNode, endOffset = sel.focusOffset;
+            sel.collapse(sel.anchorNode, sel.anchorOffset);
+
+            var direction = [];
+            if (backwards) {
+                direction = ['backward', 'forward'];
+            } else {
+                direction = ['forward', 'backward'];
+            }
+
+            sel.modify("move", direction[0], "character");
+            sel.modify("move", direction[1], "word");
+            sel.extend(endNode, endOffset);
+            sel.modify("extend", direction[1], "character");
+            sel.modify("extend", direction[0], "word");
+        }
+    } else if ( (sel = document.selection) && sel.type != "Control") {
+        var textRange = sel.createRange();
+        if (textRange.text) {
+            textRange.expand("word");
+            // Move the end back to not include the word's trailing space(s),
+            // if necessary
+            while (/\s$/.test(textRange.text)) {
+                textRange.moveEnd("character", -1);
+            }
+            textRange.select();
+        }
+    }
+}
+
   
   function getSelectionText() {
     let text = "";
@@ -10,8 +59,10 @@ javascript: (function() {
         /^(?:text|search|password|tel|url)$/i.test(activeEl.type)) &&
       (typeof activeEl.selectionStart == "number")
     ) {
+      snapSelectionToWord();
       text = activeEl.value.slice(activeEl.selectionStart, activeEl.selectionEnd);
     } else if (window.getSelection) {
+      snapSelectionToWord();
       text = window.getSelection().toString();
     }
 
