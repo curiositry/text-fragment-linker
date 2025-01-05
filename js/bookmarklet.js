1b20babc3b6a1b751c028c8f25325659b7f35655
Curiositry <isa@automathic.org>
2025-01-05 15:15:35 -0800

    Fix snapSelectionToWord so it handles trailing spaces and punctuation gracefully without selecting the following word

--- js/bookmarklet.js
+++ js/bookmarklet.js
@@ -1,54 +1,66 @@
 javascript: (function() {
 
-function snapSelectionToWord() {
-  // this function courtesy of Tim Down (author of Rangy library), creative commons licence
-  var sel;
+  function snapSelectionToWord() {
+    // this function courtesy of Tim Down (author of Rangy library), creative commons licence
+    var sel;
 
     // Check for existence of window.getSelection() and that it has a
     // modify() method. IE 9 has both selection APIs but no modify() method.
     if (window.getSelection && (sel = window.getSelection()).modify) {
-        sel = window.getSelection();
-        if (!sel.isCollapsed) {
+      sel = window.getSelection();
+      if (!sel.isCollapsed) {
 
-            // Detect if selection is backwards
-            var range = document.createRange();
-            range.setStart(sel.anchorNode, sel.anchorOffset);
-            range.setEnd(sel.focusNode, sel.focusOffset);
-            var backwards = range.collapsed;
-            range.detach();
+        // Detect if selection is backwards
+        var range = document.createRange();
+        range.setStart(sel.anchorNode, sel.anchorOffset);
+        range.setEnd(sel.focusNode, sel.focusOffset);
+        var backwards = range.collapsed;
+        range.detach();
 
-            // modify() works on the focus of the selection
-            var endNode = sel.focusNode, endOffset = sel.focusOffset;
-            sel.collapse(sel.anchorNode, sel.anchorOffset);
+        // modify() works on the focus of the selection
+        var endNode = sel.focusNode, endOffset = sel.focusOffset;
+        sel.collapse(sel.anchorNode, sel.anchorOffset);
 
-            var direction = [];
-            if (backwards) {
-                direction = ['backward', 'forward'];
-            } else {
-                direction = ['forward', 'backward'];
-            }
-
-            sel.modify("move", direction[0], "character");
-            sel.modify("move", direction[1], "word");
-            sel.extend(endNode, endOffset);
-            sel.modify("extend", direction[1], "character");
-            sel.modify("extend", direction[0], "word");
+        var direction = [];
+        if (backwards) {
+          direction = ['backward', 'forward'];
+        } else {
+          direction = ['forward', 'backward'];
         }
-    } else if ( (sel = document.selection) && sel.type != "Control") {
-        var textRange = sel.createRange();
-        if (textRange.text) {
-            textRange.expand("word");
-            // Move the end back to not include the word's trailing space(s),
-            // if necessary
-            while (/\s$/.test(textRange.text)) {
-                textRange.moveEnd("character", -1);
-            }
-            textRange.select();
+
+        sel.modify("move", direction[0], "character");
+        sel.modify("move", direction[1], "word");
+
+        sel.extend(endNode, endOffset);
+        let selStr = sel.toString();
+        sel.modify("extend", direction[1], "character");
+
+        let puncRegex = /\p{P}|\s/gu
+        // let puncRegex = /[^\w]+/g
+        if (puncRegex.test(selStr.slice(selStr.length - 1, selStr.length)) == true) {
+          sel.modify("extend", direction[1], "character");
+          // console.log("reversing");
+
+          sel.modify("extend", direction[1], "character");
+          console.log("reversing again");
         }
+        sel.modify("extend", direction[0], "word");
+      }
+    } else if ((sel = document.selection) && sel.type != "Control") {
+      var textRange = sel.createRange();
+      if (textRange.text) {
+        textRange.expand("word");
+        // Move the end back to not include the word's trailing space(s),
+        // if necessary
+        while (/\s$/.test(textRange.text)) {
+          textRange.moveEnd("character", -1);
+        }
+        textRange.select();
+      }
     }
-}
+  }
 
-  
+
   function getSelectionText() {
     let text = "";
     const activeEl = document.activeElement;
@@ -67,19 +79,19 @@ function snapSelectionToWord() {
     }
     return text;
   }
-  
+
   function updateTextSelector() {
     let selection = encodeURIComponent(getSelectionText());
     if (selection.length > 0) {
-      
+
       // URL encoded newlines are common and break text fragments. 
       if ((selection.match(/%0A/g) || []).length > 0) {
         // Split selection at newlines, use first and last new-line separated substrings
         // as start and end markers. This is what chrome seems to do also.
         let substrings = selection.split("%0A");
-        selection = substrings[0]+","+substrings[substrings.length - 1];
+        selection = substrings[0] + "," + substrings[substrings.length - 1];
       }
-      
+
       let output = url + "#:~:text=" + selection //+ start + "," + end;
       // window.location = interfaceURL+"?selection="+selection+"&url="+window.location
       navigator.clipboard.writeText(output).then(() => {
